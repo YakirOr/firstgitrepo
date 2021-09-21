f670cfd3222fc426e3551554c49e3859817d9868
YakirOr <76405915+YakirOr@users.noreply.github.com>
2021-09-21 18:17:25 +0300

    Update prepareVM.sh

--- prepareVM.sh
+++ prepareVM.sh
@@ -3,21 +3,20 @@
 yum update -y 
 pwd
 pwd
-npm install -g npm@latest
-pwd
-pwd
-pwd
+
+
 
 curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
 pwd
 yum install -y nodejs
 pwd
 yum install -y gcc-c++ make
-
+npm install -g npm@latest
 
 
 npm install -g contentful-cli
 npm audit fix --force
+
 
 cd /root
 git clone https://github.com/YakirOr/the-example-app.nodejs.git
