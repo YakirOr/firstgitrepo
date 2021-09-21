42667100ff925dde3cfa8f36ad94ba047d8b84f2
YakirOr <76405915+YakirOr@users.noreply.github.com>
2021-09-21 16:41:45 +0300

    Update prepareVM.sh

--- prepareVM.sh
+++ prepareVM.sh
@@ -1,10 +1,17 @@
 #!bin/sh
 #Prepare VM
 yum update -y 
+pwd
+pwd
 npm install -g npm@latest
+pwd
+pwd
+pwd
 
 curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+pwd
 yum install -y nodejs
+pwd
 yum install -y gcc-c++ make
 
 
