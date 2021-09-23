4d32c6aaecd495c275a06c41140173bfdcf16bec
YakirOr <76405915+YakirOr@users.noreply.github.com>
2021-09-23 10:24:53 +0300

    Update prepareVM.sh

--- prepareVM.sh
+++ prepareVM.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 #Prepare VM
-curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+curl -t --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
 yum install -y nodejs
 yum install -y gcc-c++ make
 
