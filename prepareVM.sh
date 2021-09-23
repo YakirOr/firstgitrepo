2972f65d98890ea0da592dadfc7441763eb733ab
YakirOr <76405915+YakirOr@users.noreply.github.com>
2021-09-23 09:52:38 +0300

    Update prepareVM.sh

--- prepareVM.sh
+++ prepareVM.sh
@@ -8,14 +8,3 @@ npm install -g npm@latest
 
 npm install -g contentful-cli
 npm audit fix --force
-
-echo -----------------------------------------------NewStage
-
-
-git clone https://github.com/YakirOr/the-example-app.nodejs.git
-
-npm install --prefix ./the-example-app.nodejs
-npm audit fix --force --prefix ./the-example-app.nodejs
-npm audit fix --force --prefix ./the-example-app.nodejs
-
-npm run start:dev --prefix ./the-example-app.nodejs
